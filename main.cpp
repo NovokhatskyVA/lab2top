f5542a6378d44cc9f84a976cb281d48b8e619531
Питенин Иван <ivan.pitenin@gmail.com>
2018-03-06 10:30:38 +0300

    заменил умножение делением

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+ using namespace std;
 
-using namespace std;
-//программа является учебной
-int main()
-{ int a, b;cout << "Enter A and B: ";
+ int main()
+ {
+ int a, b;
+ cout << "Enter A and B: ";
  cin >> a >> b;
-    cout << "A + B = " << a + b << '\n';
-    cout << "2*A" << 2*a << '\n';
-    cout << "A - B = " << a - b << '\n';
+ cout << "A + B = " << a + b << '\n'
+ << "A * 2 = " << a * 2 << '\n'
+ << "A - B = " << a - b << '\n'
+ << "A / B = " << a / b << '\n';
  }
